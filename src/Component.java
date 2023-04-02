17793410b623e6606951739e52963a33ba43ae5c
momenmushtaha <Mushmome1@gmail.com>
2023-04-01 22:52:05 -0400

    The factory is Working

---
+++ src/Component.java
@@ -0,0 +1,40 @@
+public class Component {
+    private String type;
+    private int inspectorId;
+    private double arrivalTime;
+    private int requiredForP1, requiredForP2, requiredForP3;
+
+    public Component(String type, int inspectorId, double arrivalTime, int requiredForP1, int requiredForP2, int requiredForP3) {
+        this.type = type;
+        this.inspectorId = inspectorId;
+        this.arrivalTime = arrivalTime;
+        this.requiredForP1 = requiredForP1;
+        this.requiredForP2 = requiredForP2;
+        this.requiredForP3 = requiredForP3;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getInspectorId() {
+        return inspectorId;
+    }
+
+    public double getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public int getRequiredForP1() {
+        return requiredForP1;
+    }
+
+    public int getRequiredForP2() {
+        return requiredForP2;
+    }
+
+    public int getRequiredForP3() {
+        return requiredForP3;
+    }
+
+}
